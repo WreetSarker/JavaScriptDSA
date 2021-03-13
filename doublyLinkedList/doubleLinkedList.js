0ffb79a6bc8bea91dcb9c334cc1ac91e29654600
WreetSarker <wreet.sarker@gmail.com>
2021-03-13 19:23:24 +0600

    added doubly linked list methods

--- doublyLinkedList/doubleLinkedList.js
+++ doublyLinkedList/doubleLinkedList.js
@@ -43,4 +43,109 @@ class DoublyLinkedList {
         this.length--;
         return currentTail;
     }
+
+    shift() {
+        if (!this.head) {
+            return undefined;
+        }
+        const oldHead = this.head;
+        if (this.length === 1) {
+            this.head = null;
+            this.tail = null;
+        } else {
+            this.head = oldHead.next;
+            this.head.prev = null;
+            oldHead.next = null;
+        }
+        this.length--;
+        return oldHead;
+    }
+
+    unshift(val) {
+        const value = new Node(val);
+        if (this.length === 0) {
+            this.head = value;
+            this.tail = value;
+        } else {
+            this.head.prev = value;
+            value.next = this.head;
+            this.head = value;
+        }
+        this.length++;
+        return this;
+    }
+
+    get(idx) {
+        if (idx < 0 || idx >= this.length) {
+            return null;
+        }
+        const mid = Math.trunc(this.length / 2);
+        if (idx <= mid) {
+            let current = this.head;
+            let count = 0;
+            while (count !== idx) {
+                current = current.next;
+                count++;
+            }
+            return current;
+        } else {
+            let current = this.tail;
+            let count = this.length - 1;
+            while (count !== idx) {
+                current = current.prev;
+                count--;
+            }
+            return current;
+        }
+    }
+
+    set(idx, val) {
+        let foundNode = this.get(idx);
+        if (foundNode !== null) {
+            foundNode.val = val;
+            return true;
+        }
+        return false;
+    }
+
+    insert(idx, val) {
+        const value = new Node(val);
+        if (idx < 0 || idx > this.length) {
+            return false;
+        } else if (idx === 0) {
+            return !!this.unshift(value);
+        } else if (idx === this.length) {
+            return !!this.push(value);
+        } else {
+            let prevNode = this.get(idx - 1);
+            let temp = prevNode.next;
+            prevNode.next = value;
+            value.prev = prevNode;
+            value.next = temp;
+            temp.prev = value;
+            this.length++;
+            return true;
+        }
+    }
+
+    remove(idx) {
+        if (idx < 0 || idx >= this.length) {
+            return undefined;
+        } else if (idx === 0) {
+            return this.shift();
+        } else if (idx === this.length - 1) {
+            return this.pop();
+        } else {
+            let current = this.get(idx);
+            let temp = current.next;
+            current.prev.next = temp;
+            temp.prev = current.prev;
+
+            current.prev = null;
+            current.next = null;
+            this.length--;
+            return current;
+        }
+    }
 }
+
